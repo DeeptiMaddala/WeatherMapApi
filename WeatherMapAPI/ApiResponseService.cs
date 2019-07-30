955f0f776d531d33fcab75ae62c662a93d88464e
deepti.maddala <deepti.maddala@gmail.com>
2019-07-30 21:24:26 +1000

    Add project files.

---
+++ WeatherMapAPI/ApiResponseService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMapAPI
+{
+    public class ApiResponseService
+    {
+        private string ApiUrl = ConfigurationManager.AppSettings["apiurl"];
+        private string appid = ConfigurationManager.AppSettings["APPID"];
+        public string getResponse(string City, string NoOfDays)
+        {
+            StringBuilder ApiResponseString = new StringBuilder();
+            string Mode = "JSON";
+            string Units = "Metric";
+
+            //string UrlParameters = City + "&APPID=" + appid + "&cnt=" + NoOfDays + "&mode=" + Mode + "&units=" + Units;
+
+            string UrlParameters = City + "&appid=" + appid;
+
+            ApiUrl = String.Concat(ApiUrl, UrlParameters);
+            Console.WriteLine(ApiUrl);
+            string jsonResponse;
+            using (WebClient client = new WebClient())
+            {
+                client.Headers.Add(HttpRequestHeader.ContentType, "application/json");
+                jsonResponse = client.DownloadString(ApiUrl);
+                if (jsonResponse == null)
+                {
+                    jsonResponse = "error";
+                }
+            }
+
+            return jsonResponse;
+        }
+    }
+}
---
+++ WeatherMapAPI/ApiTests/HappyHolidayTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherMapAPI.ApiTests
+{
+    public class HappyHolidayTest
+    {
+        [Test]
+        public void PlanHoliday()
+        {
+            ApiResponseService Api = new ApiResponseService();
+            string responseCityForcast = Api.getResponse(ConfigurationManager.AppSettings["cityname"], ConfigurationManager.AppSettings["noofdays"]);
+            Console.WriteLine(responseCityForcast.ToString());
+            Assert.IsTrue(responseCityForcast.Contains(ConfigurationManager.AppSettings["cityname"]));
+
+            
+        }
+    }
+}
